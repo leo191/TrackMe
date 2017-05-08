3b7b7246f4277b37aa62310b2d626fb86b371f34
Subhadip Banerjee <subhadipbanerjee.527@gmail.com>
2017-05-09 00:36:26 +0530

    basic working

--- app/src/main/java/com/example/leo/trackme/MapsActivity.java
+++ app/src/main/java/com/example/leo/trackme/MapsActivity.java
@@ -1,27 +1,36 @@
 package com.example.leo.trackme;
 
-import android.*;
-import android.animation.ValueAnimator;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Point;
+
 import android.location.Location;
-import android.os.AsyncTask;
+
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.SystemClock;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.BounceInterpolator;
 import android.view.animation.Interpolator;
-import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -49,6 +58,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
+
 import static com.example.leo.trackme.R.id.map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
@@ -58,20 +68,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //Basic variables
     String searchfor,whoru;
     Double vlat,vlong;
-    double latitude, longitude;
+    Double latitude, longitude;
     //Views Variables
     Button btTrack;
     EditText edsearch, edusername;
     DatabaseReference dRef;
     Button pl,min;
-    LatLng lo;
+    LatLng last_curr_pos;
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
     private FusedLocationProviderApi locationProviderApi = LocationServices.FusedLocationApi;
     TextView ltlng;
     LocationOfUser usr;
     private GoogleMap mMap;
-
+    RadioGroup radioGroup;
+    ImageView normal,satalit;
+    View mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,9 +91,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         setContentView(R.layout.activity_maps);
-
-        //Supppor for M and later
-
+        //getWindow().getDecorView().setSystemUiVisibility(
+            //    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
 
         //Google api client for FusedLocationProvider
         googleApiClient = new GoogleApiClient.Builder(this).
@@ -103,7 +119,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(map);
         mapFragment.getMapAsync(this);
-
+        mapView = mapFragment.getView();
 
 
 //Basic UI and Initialization
@@ -118,6 +134,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         min = (Button)findViewById(R.id.sub);
 
 
+        normal =(ImageView)findViewById(R.id.normal_map_button);
+        satalit = (ImageView) findViewById(R.id.satalite_map_button);
+
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mMap!=null)
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        satalit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mMap!=null)
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+        });
        /* pl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -167,6 +201,107 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+//        if(mMap!=null)
+//        {
+//            mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+//                @Override
+//                public boolean onMyLocationButtonClick() {
+//                    if(latitude == null&& longitude == null)
+//                    {
+//
+//                    }
+//                    LatLng target = new LatLng(latitude, longitude);
+//                    CameraPosition position = mMap.getCameraPosition();
+//
+//                    CameraPosition.Builder builder = new CameraPosition.Builder();
+//                    builder.zoom(15);
+//                    builder.target(target);
+//
+//                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(builder.build()));
+//                    return  true;
+//
+//                }
+//
+//            });
+//        }
+
+
+       checkforNetworkAndGps();
+
+    }
+
+    private void checkforNetworkAndGps() {
+
+        LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+
+        if(lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+
+        }
+        else {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle);
+            builder.setTitle("Need Location");
+            builder.setMessage("We Require Location access to Continue Tracking.... ");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+
+
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.setCancelable(false);
+            builder.show();
+
+
+        }
+
+
+
+
+        /*boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            // notify user
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setMessage(context.getResources().getString(R.string.gps_network_not_enabled));
+            dialog.setPositiveButton(context.getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    context.startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton(context.getString(R.string.Cancel), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    finish();
+
+                }
+            });
+            dialog.show();
+        }*/
     }
 
 
@@ -183,59 +318,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onDataChange(DataSnapshot dataSnapshot) {
                 usr = dataSnapshot.getValue(LocationOfUser.class);
 
-                final LatLng opp = new LatLng(usr.getLati(), usr.getLongi());
+                final LatLng first_curr_pos = new LatLng(usr.getLati(), usr.getLongi());
                 mMap.clear();
-                final Marker mrk = mMap.addMarker(new MarkerOptions().position(opp).title("Marker in " + edsearch.getText().toString() + " ass"));
+                final Marker mrk = mMap.addMarker(new MarkerOptions().position(first_curr_pos).title("Marker in " + edsearch.getText().toString() + " ass"));
 
-                //Handler for Animating Marker
-                if(lo!=null) {
-                    final LatLng startPosition = lo;
-                    final LatLng finalPosition = opp;
-                    final Handler handler = new Handler();
-                    final long start = SystemClock.uptimeMillis();
-                    final Interpolator interpolator = new AccelerateDecelerateInterpolator();
-                    final float durationInMs = 3000;
-                    final boolean hideMarker = false;
-
-                    handler.post(new Runnable() {
-                        long elapsed;
-                        float t;
-                        float v;
-
-                        @Override
-                        public void run() {
-                            // Calculate progress using interpolator
-                            elapsed = SystemClock.uptimeMillis() - start;
-                            t = elapsed / durationInMs;
-                            v = interpolator.getInterpolation(t);
-
-                            LatLng currentPosition = new LatLng(
-                                    startPosition.latitude * (1 - t) + finalPosition.latitude * t,
-                                    startPosition.longitude * (1 - t) + finalPosition.longitude * t);
-
-
-                            mrk.setPosition(currentPosition);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(opp));
-                            // Repeat till progress is complete.
-                            if (t < 1) {
-                                // Post again 16ms later.
-                                handler.postDelayed(this, 16);
-                            } else {
-                                if (hideMarker) {
-                                    mrk.setVisible(false);
-                                } else {
-                                    mrk.setVisible(true);
-                                }
-                            }
-                        }
-                    });
-                }
-                lo=opp;
-
-
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(first_curr_pos));
                 mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
 
 
+                //Handler for Animating Marker
+                if(last_curr_pos!=null) {
+                    animateMarker(last_curr_pos,first_curr_pos,mrk);
+                }
+                last_curr_pos=first_curr_pos;
 
 
 
@@ -251,9 +346,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+//animating marker smoothly
+
+void animateMarker(LatLng startltln, LatLng endltln, final Marker marker)
+{
+    final LatLng startPosition = startltln;
+    final LatLng finalPosition = endltln;
+    final Handler handler = new Handler();
+    final long start = SystemClock.uptimeMillis();
+    final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+    final float durationInMs = 3000;
+    final boolean hideMarker = false;
+
+    handler.post(new Runnable() {
+        long elapsed;
+        float t;
+        float v;
+
+        @Override
+        public void run() {
+            // Calculate progress using interpolator
+            elapsed = SystemClock.uptimeMillis() - start;
+            t = elapsed / durationInMs;
+            v = interpolator.getInterpolation(t);
+
+            LatLng currentPosition = new LatLng(
+                    startPosition.latitude * (1 - t) + finalPosition.latitude * t,
+                    startPosition.longitude * (1 - t) + finalPosition.longitude * t);
 
 
+            marker.setPosition(currentPosition);
+            // Repeat till progress is complete.
+            if (t < 1) {
+                // Post again 16ms later.
+                handler.postDelayed(this, 16);
+            } else {
+                if (hideMarker) {
+                    marker.setVisible(false);
+                } else {
+                    marker.setVisible(true);
+                }
+            }
+        }
+    });
 
+}
 
 
 
@@ -271,13 +408,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setBuildingsEnabled(true);
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        //mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             return;
         }
+
+
+        if (mapView != null &&
+                mapView.findViewById(Integer.parseInt("1")) != null) {
+            // Get the button view
+            View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+            // and next place it, on bottom right (as Google Maps app)
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationButton.getLayoutParams();
+            // position on right bottom
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 100);
+        }
+        //mMap.setPadding(0,displayMetrics.heightPixels-500,0,0);
+
         mMap.setMyLocationEnabled(true);
 
     }
@@ -341,6 +496,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onPause() {
         super.onPause();
-        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+        requestLocationUp();
     }
 }
